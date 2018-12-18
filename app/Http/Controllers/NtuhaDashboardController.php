6ad101be5dea2111e7e710944c02f933a51f5281
Thembo Charles Lwanga <ashley7520charles@gmail.com>
2018-12-18 11:03:33 +0300

    removes the burd in the API

--- app/Http/Controllers/NtuhaDashboardController.php
+++ app/Http/Controllers/NtuhaDashboardController.php
@@ -12,26 +12,22 @@ class NtuhaDashboardController extends Controller
 {
 
 
-    /** 
-
-     * @connections Connection to the database
-     * @return a database instance
-
-    */     
-
-    public static function connection_to_firebase()
-    {
-        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/ntuhatransport-firebase-adminsdk-9e7cu-56ffdea3cf.json');
-        $firebase = (new Factory)->withServiceAccount($serviceAccount)->withDatabaseUri('https://ntuhatransport.firebaseio.com/')->create();       
-        return $firebase->getDatabase();
-    }
-
     /*
       returns all customers
     */
     public static function read_ntuha_customers()
     {
-        $reference = $this->connection_to_firebase()->getReference('Users');
+        /** 
+
+         * @connections Connection to the database
+         * @return a database instance
+
+        */     
+        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/ntuhatransport-firebase-adminsdk-9e7cu-56ffdea3cf.json');
+        $firebase = (new Factory)->withServiceAccount($serviceAccount)->withDatabaseUri('https://ntuhatransport.firebaseio.com/')->create();
+        $database = $firebase->getDatabase();
+
+        $reference = $database->getReference('Users');
         $customers = $reference->getChild("Customers");
         if (!empty($customers->getValue())) {
             $customer_data = [];
@@ -62,7 +58,11 @@ class NtuhaDashboardController extends Controller
     */
     public static function read_ntuha_drivers()
     {
-        $reference = $this->connection_to_firebase()->getReference('Users');
+        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/ntuhatransport-firebase-adminsdk-9e7cu-56ffdea3cf.json');
+        $firebase = (new Factory)->withServiceAccount($serviceAccount)->withDatabaseUri('https://ntuhatransport.firebaseio.com/')->create();
+        $database = $firebase->getDatabase();
+
+        $reference = $database->getReference('Users');
         $drivers = $reference->getChild("Drivers");
         if (!empty($drivers->getValue())) {
             $driver_data = [];
@@ -103,14 +103,19 @@ class NtuhaDashboardController extends Controller
     */
     public static function single_user_history($user_type,$user_id)
     {
-        $driver = $this->user_details($user_type,$user_id);
+
+        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/ntuhatransport-firebase-adminsdk-9e7cu-56ffdea3cf.json');
+        $firebase = (new Factory)->withServiceAccount($serviceAccount)->withDatabaseUri('https://ntuhatransport.firebaseio.com/')->create();
+        $database = $firebase->getDatabase();
+
+        $driver = NtuhaDashboardController::user_details($user_type,$user_id);
         $history_data = array();
         foreach ($driver as $data_key => $data_value) {
             // for each history, read details
             if (!empty($data_value['history'])) {
             
                 foreach ($data_value['history'] as $key_data => $key_value) {
-                    $history_refrence_reference = $this->connection_to_firebase()->getReference('history')->getChild($key_data)->getValue();
+                    $history_refrence_reference = $database->getReference('history')->getChild($key_data)->getValue();
 
                     array_push($history_data,$history_refrence_reference);
 
@@ -132,7 +137,11 @@ class NtuhaDashboardController extends Controller
     public static function user_details($user_type,$user_id)
     {
         $data = array();
-        $reference = $this->connection_to_firebase()->getReference('Users');
+        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/ntuhatransport-firebase-adminsdk-9e7cu-56ffdea3cf.json');
+        $firebase = (new Factory)->withServiceAccount($serviceAccount)->withDatabaseUri('https://ntuhatransport.firebaseio.com/')->create();
+        $database = $firebase->getDatabase();
+
+        $reference = $database->getReference('Users');
         $user = $reference->getChild($user_type)->orderByKey()->equalTo($user_id)->getValue();
         return $user;//this is a single object for one user
     }
@@ -140,7 +149,7 @@ class NtuhaDashboardController extends Controller
     public static function single_customer($customer_id)
     {
         $data = array();
-         foreach ($this->user_details("Customers",$customer_id) as $key => $value) {
+         foreach (NtuhaDashboardController::user_details("Customers",$customer_id) as $key => $value) {
             $result = [];
             try {
                 $result['name'] = $value['name'];
@@ -157,7 +166,7 @@ class NtuhaDashboardController extends Controller
     public static function single_driver($driver_id)
     {
         $data = array();
-         foreach ($this->user_details("Drivers",$driver_id) as $key => $value) {
+         foreach (NtuhaDashboardController::user_details("Drivers",$driver_id) as $key => $value) {
             $result = [];
             try {
                $result['name'] = $value['name'];
@@ -181,12 +190,17 @@ class NtuhaDashboardController extends Controller
     public static function rides()
     {
         $rides_data = array();
-        $rides = $this->connection_to_firebase()->getReference('history')->getValue();
+
+        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/ntuhatransport-firebase-adminsdk-9e7cu-56ffdea3cf.json');
+        $firebase = (new Factory)->withServiceAccount($serviceAccount)->withDatabaseUri('https://ntuhatransport.firebaseio.com/')->create();
+        $database = $firebase->getDatabase();
+
+        $rides = $database->getReference('history')->getValue();
         foreach ($rides as $key_key => $ride_value) {
             $result = [];
-            $result["customer"] = $this->single_customer($ride_value['customer']);
+            $result["customer"] = NtuhaDashboardController::single_customer($ride_value['customer']);
             $result["distance"] = $ride_value['distance'];
-            $result["driver"] = $this->single_driver($ride_value['driver']);
+            $result["driver"] = NtuhaDashboardController::single_driver($ride_value['driver']);
             $result["location"] = (array)$ride_value['location'];
             $result["rating"] = $ride_value['rating'];
             $result["timestamp"] = $ride_value['timestamp'];
@@ -202,9 +216,14 @@ class NtuhaDashboardController extends Controller
     public static function drivers_available()
     {
         $rides_data = array();
-        $rides = $this->connection_to_firebase()->getReference('driversAvailable')->getValue();
+
+        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/ntuhatransport-firebase-adminsdk-9e7cu-56ffdea3cf.json');
+        $firebase = (new Factory)->withServiceAccount($serviceAccount)->withDatabaseUri('https://ntuhatransport.firebaseio.com/')->create();
+        $database = $firebase->getDatabase();
+
+        $rides = $database->getReference('driversAvailable')->getValue();
         foreach ($rides as $key => $driver_value) {
-            array_push($rides_data, $this->single_driver($key));
+            array_push($rides_data, NtuhaDashboardController::single_driver($key));
         }
         return response()->json($rides_data);
     }
@@ -216,10 +235,14 @@ class NtuhaDashboardController extends Controller
    public static function working_drivers()
    {
         $rides_data = array();
-        $rides = $this->connection_to_firebase()->getReference('workingDrivers');
+        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/ntuhatransport-firebase-adminsdk-9e7cu-56ffdea3cf.json');
+        $firebase = (new Factory)->withServiceAccount($serviceAccount)->withDatabaseUri('https://ntuhatransport.firebaseio.com/')->create();
+        $database = $firebase->getDatabase();
+
+        $rides = $database->getReference('workingDrivers');
         $working_drivers = $rides->getValue();
         foreach ($rides as $key => $driver_value) {
-            array_push($rides_data, $this->single_driver($key));
+            array_push($rides_data, NtuhaDashboardController::single_driver($key));
         }
         return response()->json($rides_data);         
     }
@@ -236,7 +259,7 @@ class NtuhaDashboardController extends Controller
 */
     public function index()
     {  
-        return $this->working_drivers();
+         
     }    
 
     /**
@@ -246,7 +269,8 @@ class NtuhaDashboardController extends Controller
      */
     public function create()
     {
-        //
+ 
+   
     }
 
     /**
