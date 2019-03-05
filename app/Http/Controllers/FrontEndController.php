33a603c23ad18c2afb06f3006ff79164ccefc443
THEMBO CHARLES LWANGA <ashley7520charles@gmail.com>
2019-03-05 16:25:41 +0300

    Cleaning payment, success to successful

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -213,7 +213,7 @@ class FrontEndController extends Controller
     public static function account_balance(Request $request)
     {
         // $payments = Payment::where('email',$request->email)->sum('amount');
-        $payments = Payment::where('email',$request->email)->where('status','success')->sum('amount');
+        $payments = Payment::where('email',$request->email)->where('status','successful')->sum('amount');
         $with_draw = Withdraw::where('email',$request->email)->sum('amount');
         return ($payments - $with_draw);        
     }
