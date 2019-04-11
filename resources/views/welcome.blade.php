2a6d07125c43891a488dcea4baf3e0213ff8a33d
THEMBO CHARLES LWANGA <ashley7520charles@gmail.com>
2019-04-11 14:55:46 +0300

    Adds Logoes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Ntuha</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="description" content="A fully featured transport system.">
+        <meta name="author" content="Thembo Charles Lwanga">
+        <meta name="author_email" content="ashley7520charles@gmail.com">
+
+        <link rel="icon" href="{{asset('logo/splash.jpg')}}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -81,7 +88,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    <img src="{{asset('logo/logo.png')}}">
+                    <img src="{{asset('logo/logo.png')}}" width="40%">
                 </div>
 
                 <div class="links">
