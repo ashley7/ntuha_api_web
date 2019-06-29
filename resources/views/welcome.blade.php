dfbd2d1b70109cde6dae2c2cab5c333ae43c84f6
THEMBO CHARLES LWANGA <ashley7520charles@gmail.com>
2019-06-29 15:09:12 +0300

    Adds home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,201 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="description" content="A fully featured transport system.">
-        <meta name="author" content="Thembo Charles Lwanga">
-        <meta name="author_email" content="ashley7520charles@gmail.com">
+<head>
+  <meta charset="utf-8">
+  <title>Welcome To Ntuha Uganda</title>
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width">
+  
+ 
 
-        <link rel="icon" href="{{asset('logo/splash.jpg')}}">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="description" content="A fully featured transport system.">
+  <meta name="author" content="Thembo Charles Lwanga">
+  <meta name="author_email" content="ashley7520charles@gmail.com">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+  <link rel="icon" href="{{asset('logo/splash.jpg')}}">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+  <!-- Styles -->
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/animate.css">
+  <link href='http://fonts.googleapis.com/css?family=Dr+Sugiyama|Lato:400,100,100italic,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+  
 
-            .full-height {
-                height: 100vh;
-            }
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/main.css">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+  <script src="js/modernizr-2.7.1.js"></script>
 
-            .position-ref {
-                position: relative;
-            }
+  <!-- Fav and touch icons -->
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="/apple-touch-icon-144-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/apple-touch-icon-114-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/apple-touch-icon-72-precomposed.png">
+  <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-57-precomposed.png">
+</head>
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+<body>
 
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <!-- <a href="{{ route('register') }}">Register</a> -->
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    <img src="{{asset('logo/logo.png')}}" width="40%">
-                </div>
-
-                <div class="links">                    
-                    This webpage is under maintenance                
-                </div>
-            </div>
+    <div class="navbar navbar-inverse navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href=""><img src="images/ntuhalogo.png" alt="Logo"></a>
         </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="">Home</a></li>
+           <!--  <li><a href="#about">About</a></li>
+            <li><a href="#Ride">Ntuha Ride</a></li> -->
+            <li><a href="/login">Login</a></li>
+          </ul>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </div>
+
+    <!--Header-->
+    <section id="header">
+      <div class="container">
+        <div class="row header-text">
+          <div class="col-sm-8 pull-right text-right">
+           <div class="margin-100">
+            <h1>We're Ntuda,</h1>
+            <h3>Reliable, Fast and Safe <strong><em>free</em></strong> too!</h3>
+           </div>
+            <a href="http://tiny.cc/wywc7y" class="btn btn-primary btn-lg">Download <i class="fa fa-cloud-download"></i></a>
+          </div>
+        </div>
+        
+        <div class="row">
+          <div class="col-sm-4 text-center wow fadeInUp">
+            <img class="header-img" src="images/ntuha-promo.jpg" alt="Header Image">
+          </div>
+        </div>
+        
+      </div>
+    </section>
+    
+    
+    <!--Main 1-->
+    <section id="main1">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6 margin-30">
+            <h2>What is Ntuha Ride?</h2><br />
+            <p>Ntuha ride is a transportation service company which deals in motorbikes, cabs/special hire taxis and trucks.  Customers can order for any of the above transport of their choice through the Ntuha ride app which is available on google play or call our service center to be assisted. This app is geo-location enabled thus capturing the customers pick up address and drop off address which eases navigation. The drivers are well trained with licenses and have vast knowledge of the region with backup help from their rider apps. The motorbikes are fitted with helmets to ensure your safety and comfort. </p><br />
+            <blockquote>
+              <br>How to use it </br>
+                1.  Use the Ntuha Ride app<br>
+                 Download the Ntuha Ride app from google play<br>
+                 Register with your phone number <br>
+                 Input your pick up and drop off address<br>
+                 select your desired mode of transport i.e. Ntuha boda, Ntuha taxi or ntuha truck<br>
+                 confirm payment mode i.e. cash or account<br>
+                 Request a ride<br>
+                 Sit back and let’s take you around<br>
+                <br>2.  Call our helpline +256704444481</br>
+                <br>Why ride with ntuha?</br>
+                Get a safe ride in minutes, at any time.
+                Schedule a ride for any day at ease
+                Budget friendly prices to your location
+                An easy way to get around from anywhere in Mbarara
+            </blockquote>
+            <br />
+            <a href="mailto:ntuhadeliveries@gmail.com" class="btn btn-secondary btn-lg">Get in Touch <i class="fa fa-envelope"></i></a>
+          </div>
+          
+          <div class="col-sm-6 wow fadeInUpBig">
+            <img class="main1-img img-responsive pull-right" src="images/checkout.jpg" alt="Main1 Image">
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    <!--Main 2-->
+    <section id="main2">
+      <div class="container">
+        <div class="row">
+          
+          <div class="col-sm-6 wow fadeInUpBig">
+            <img class="main2-img pull-left" src="images/move.jpg" alt="Main2 Image">
+          </div>
+        
+          <div class="col-sm-6 margin-30">
+            <h2>Where We Are</h2><br />
+            <p>Using Ntuha Ride is an easy way to get around from anywhere in Mbarara City, the biggest City in Western Uganda</p><br />
+            
+            <br />
+            <a href="https://www.google.com/search?q=google+map+for+mbarara&rlz=1C1RLNS_enUG816UG817&oq=google+map+for+mbarara+&aqs=chrome..69i57j69i64.26244j0j4&sourceid=chrome&ie=UTF-8#" class="btn btn-secondary btn-lg">View our Coverage <i class="fa fa-dribbble"></i></a>
+          </div>
+
+        </div>
+      </div>
+    </section>
+    <!--Main 3-->
+    <section id="main2">
+      <div class="container">
+            <h2>Who are we?</h2><br />
+            <p>Ntuha services is a high-tech transport company situated in the heart of Mbarara town in western Uganda. Ntuha is here to bring a drastic reliable and fabulous change to the transport system in western Uganda like never seen before. Ntuha started with a food delivery service known as Ntuha food in May 2017 which took Mbarara by a storm. Ntuha food partners with all the best restaurants and grocery stores in the region. <br>
+            Over the course of time, other services like Ntuha ride and Ntuha tours and travel were introduced, and we must say, “Mbarara needed this” as reviewed by a client
+            We have developed our very own technology systems and applications to ensure a smooth end to end reliable experience. We also have a daily running customer care center to assist with your queries.
+            We understand the significance of professional service delivery thus; our teams go through various trainings to bring you the best service in the region.
+            </p>
+
+        
+      </div>
+    </section>
+    <!--Message-->
+    <section id="message">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <h2 class="white">NTUHA TOURS AND TRAVEL</h2>
+          <p><br>Ntuha safaris is a travel agency based in Mbarara offering amazing safari transport services. We have identified ourselves as one of the most trusted and reliable tour agency delivering an adventurous, memorable and cost-effective safaris.
+           Transportation </p>
+         <p> We have a wide range of safari vehicles fitting for all categories of clientele. Our vehicles are 4WD vehicles safe for any type of terrain. The safari trucks have a pop-up roof making it ideal for game viewing. The vehicles are well maintained to provide maximum comfort and safety, and are well equipped with cool boxes and first aid kits. </p>
+           Our drivers
+          All our drivers are well versed with English and have knowledge of the local languages to ease communication. The success of your safari largely depends on your driver/guide hence our team is carefully selected, well trained and experienced with a vast knowledge of east African flora and fauna.
+          We hope you will visit the pearl of Africa with us
+</p><br />
+            <a href="http://tiny.cc/wywc7y" class="btn btn-primary btn-lg">Ride with Us Today<i class="fa fa-bike"></i></a>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+ 
+    
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-12 text-center">
+            <p><small>Copyright &copy; 2019: Ntuha Uganda. All rights reserved.</small></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    
+    <!-- Javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-2.1.0.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/main.js"></script>
+    
+    <script>
+      new WOW().init();
+    </script>
+    
+   
     </body>
 </html>
