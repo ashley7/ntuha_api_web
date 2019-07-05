77d3da1e91710de4b11077c63a6021c5cccc5874
THEMBO CHARLES LWANGA <ashley7520charles@gmail.com>
2019-07-05 10:00:37 +0300

    Editd home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,24 +1,16 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
+<!DOCTYPE html>
+<html lang="en"><head>
   <meta charset="utf-8">
   <title>Welcome To Ntuha Uganda</title>
   <meta name="keywords" content="">
   <meta name="description" content="">
   <meta name="viewport" content="width=device-width">
   
- 
-
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-  <meta name="description" content="A fully featured transport system.">
-  <meta name="author" content="Thembo Charles Lwanga">
-  <meta name="author_email" content="ashley7520charles@gmail.com">
-
-  <link rel="icon" href="{{asset('logo/splash.jpg')}}">
+  <meta property="og:title" content="">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="">
+  <meta property="og:site_name" content="">
+  <meta property="og:description" content="">
 
   <!-- Styles -->
   <link rel="stylesheet" href="css/font-awesome.min.css">
@@ -36,6 +28,7 @@
   <link rel="apple-touch-icon-precomposed" sizes="114x114" href="/apple-touch-icon-114-precomposed.png">
   <link rel="apple-touch-icon-precomposed" sizes="72x72" href="/apple-touch-icon-72-precomposed.png">
   <link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-57-precomposed.png">
+  <link rel="shortcut icon" href="/favicon.ico">
 </head>
 
 <body>
@@ -48,14 +41,15 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href=""><img src="images/ntuhalogo.png" alt="Logo"></a>
+          <a class="navbar-brand" href="index.html"><img src="img/ntuha.png" alt="Logo"></a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="">Home</a></li>
-           <!--  <li><a href="#about">About</a></li>
-            <li><a href="#Ride">Ntuha Ride</a></li> -->
+            <li><a href="index.html">Home</a></li>
             <li><a href="/login">Login</a></li>
+            <!-- <li><a href="#about">About</a></li>
+            <li><a href="#Ride">Ntuha Ride</a></li>
+            <li><a href="#contact">Contact</a></li> -->
           </ul>
         </div><!--/.navbar-collapse -->
       </div>
@@ -67,7 +61,7 @@
         <div class="row header-text">
           <div class="col-sm-8 pull-right text-right">
            <div class="margin-100">
-            <h1>We're Ntuda,</h1>
+            <h1>We're Ntuha,</h1>
             <h3>Reliable, Fast and Safe <strong><em>free</em></strong> too!</h3>
            </div>
             <a href="http://tiny.cc/wywc7y" class="btn btn-primary btn-lg">Download <i class="fa fa-cloud-download"></i></a>
@@ -76,7 +70,7 @@
         
         <div class="row">
           <div class="col-sm-4 text-center wow fadeInUp">
-            <img class="header-img" src="images/ntuha-promo.jpg" alt="Header Image">
+            <img class="header-img" src="img/ntuha-promo.jpg" alt="Header Image">
           </div>
         </div>
         
@@ -100,7 +94,7 @@
                  select your desired mode of transport i.e. Ntuha boda, Ntuha taxi or ntuha truck<br>
                  confirm payment mode i.e. cash or account<br>
                  Request a ride<br>
-                 Sit back and let’s take you around<br>
+                 Sit back and let’s take you around
                 <br>2.  Call our helpline +256704444481</br>
                 <br>Why ride with ntuha?</br>
                 Get a safe ride in minutes, at any time.
@@ -113,7 +107,7 @@
           </div>
           
           <div class="col-sm-6 wow fadeInUpBig">
-            <img class="main1-img img-responsive pull-right" src="images/checkout.jpg" alt="Main1 Image">
+            <img class="main1-img img-responsive pull-right" src="img/checkout.jpg" alt="Main1 Image">
           </div>
         </div>
       </div>
@@ -125,7 +119,7 @@
         <div class="row">
           
           <div class="col-sm-6 wow fadeInUpBig">
-            <img class="main2-img pull-left" src="images/move.jpg" alt="Main2 Image">
+            <img class="main2-img pull-left" src="img/move.jpg" alt="Main2 Image">
           </div>
         
           <div class="col-sm-6 margin-30">
@@ -140,16 +134,14 @@
       </div>
     </section>
     <!--Main 3-->
-    <section id="main2">
+<section id="main2">
       <div class="container">
             <h2>Who are we?</h2><br />
-            <p>Ntuha services is a high-tech transport company situated in the heart of Mbarara town in western Uganda. Ntuha is here to bring a drastic reliable and fabulous change to the transport system in western Uganda like never seen before. Ntuha started with a food delivery service known as Ntuha food in May 2017 which took Mbarara by a storm. Ntuha food partners with all the best restaurants and grocery stores in the region. <br>
+            <p>Ntuha services is a high-tech transport company situated in the heart of Mbarara town in western Uganda. Ntuha is here to bring a drastic reliable and fabulous change to the transport system in western Uganda like never seen before. Ntuha started with a food delivery service known as Ntuha food in May 2017 which took Mbarara by a storm. Ntuha food partners with all the best restaurants and grocery stores in the region. 
             Over the course of time, other services like Ntuha ride and Ntuha tours and travel were introduced, and we must say, “Mbarara needed this” as reviewed by a client
             We have developed our very own technology systems and applications to ensure a smooth end to end reliable experience. We also have a daily running customer care center to assist with your queries.
             We understand the significance of professional service delivery thus; our teams go through various trainings to bring you the best service in the region.
-            </p>
-
-        
+            </p><br />
       </div>
     </section>
     <!--Message-->
@@ -158,19 +150,21 @@
         <div class="row">
           <div class="col-sm-12 text-center">
             <h2 class="white">NTUHA TOURS AND TRAVEL</h2>
-          <p><br>Ntuha safaris is a travel agency based in Mbarara offering amazing safari transport services. We have identified ourselves as one of the most trusted and reliable tour agency delivering an adventurous, memorable and cost-effective safaris.
-           Transportation </p>
-         <p> We have a wide range of safari vehicles fitting for all categories of clientele. Our vehicles are 4WD vehicles safe for any type of terrain. The safari trucks have a pop-up roof making it ideal for game viewing. The vehicles are well maintained to provide maximum comfort and safety, and are well equipped with cool boxes and first aid kits. </p>
+          <p><br>Ntuha safaris is a travel agency based in Mbarara offering amazing safari transport services. We have identified ourselves as one of the most trusted and reliable tour agency delivering an adventurous, memorable and cost-effective safaris.<br>
+           Transportation
+          We have a wide range of safari vehicles fitting for all categories of clientele. Our vehicles are 4WD vehicles safe for any type of terrain. The safari trucks have a pop-up roof making it ideal for game viewing. The vehicles are well maintained to provide maximum comfort and safety, and are well equipped with cool boxes and first aid kits.<br>
            Our drivers
           All our drivers are well versed with English and have knowledge of the local languages to ease communication. The success of your safari largely depends on your driver/guide hence our team is carefully selected, well trained and experienced with a vast knowledge of east African flora and fauna.
           We hope you will visit the pearl of Africa with us
-</p><br />
+          </p><br />
             <a href="http://tiny.cc/wywc7y" class="btn btn-primary btn-lg">Ride with Us Today<i class="fa fa-bike"></i></a>
           </div>
         </div>
       </div>
     </section>
     
+    
+    <!--Bottom-->
  
     
     <footer>
