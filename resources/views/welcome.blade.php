c66981d01c4b63f1e24b155de038db76bfcb62d7
THEMBO CHARLES LWANGA <ashley7520charles@gmail.com>
2019-07-19 10:48:37 +0300

    Adds privacy policy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,31 @@
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/main.css">
 
+  <style>
+.collapsible {
+  background-color: #777;
+  color: white;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+}
+
+.active, .collapsible:hover {
+  background-color: #555;
+}
+
+.content {
+  padding: 0 18px;
+  display: none;
+  overflow: hidden;
+  background-color: #f1f1f1;
+}
+</style>
+
   <script src="js/modernizr-2.7.1.js"></script>
 
   <!-- Fav and touch icons -->
@@ -162,9 +187,112 @@
         </div>
       </div>
     </section>
+
+        <!--Privacy Policy Section-->
+    <section id="main">
+      <div class="container">
+            <h2>PRIVACY POLICIES</h2><br />
+            <button class="collapsible">Ntuha Ride App Users' Privacy Policy</button>
+        <div class="content">
+          <p>Ntuha Ride located at Plot 11a KAR drive, Kampala Uganda, is the controller of personal data of customers collected from all its online platforms.
+        The term "us" or "we" refers to the owner of the Ntuha Ride app, Ntuha Ride, is a private limited company, founded in the Republic of Uganda.<br>
+        1. Personal data we process<br>
+        • Name, phone number, e-mail address.<br>
+        • Geolocation of the passenger, the time and the destination of a journey.<br>
+        • Payment information.<br>
+        • Identification data of the device on which the Ntuha Ride app has been installed.<br>
+        2. Purpose of the process<br>
+        • We collect and process personal data for the purpose of connecting passengers with drivers to help them move around cities more efficiently.<br>
+        • We display geolocation data and the phone number of passengers to drivers to enable efficient pick-up. Geolocation data is collected only when the Ntuha Ride app is activated. The collection of geolocation data stops after closing the Ntuha Ride app. <br>
+        • We may use geolocation data to resolve quality issues related to transportation services.
+        • We use contact details to notify passengers of updates to the Ntuha Ride app.
+        • We collect data of the routes taken by drivers and passengers to analyse the geographic coverage in order to provide recommendations to the drivers about most efficient routes.
+        • Your name, phone number and e-mail will be used to communicate with you.
+        • We obtain payment details to process passengers' payment on behalf of drivers for transportation services.
+        • Customer support data is collected on a case-by-case basis and stored for the purpose of resolving disputes and service quality issues.
+        3. Legal Basis
+        • Personal data is processed in order to provide the service contracted with passengers. We collect and processes the personal data submitted by the passengers in the course of installation and use of the Ntuha Ride app. The prerequisite for the use of Ntuha services is passengers agreeing to the processing of identification and geolocation data.
+        • Personal data may be also processed on legitimate interest grounds, for example in investigating and detecting fraudulent payments.
+        4. Recipients
+        • The personal data of passenger is only disclosed to driver who has activated Ntuha Ride app; in such case, driver will see the name, phone number and geolocation data of the passenger.
+        • After providing the transportation service, the name and the telephone number of the passenger will remain visible to the driver for 24 hours. This is necessary for drivers to resolve any issues associated with service provision, e.g., to contact the passenger if something was left behind in the vehicle.
+        • Feedback given by passengers regarding the quality of the service is anonymous and drivers do not receive names and telephone numbers of the passenger who provided rating and feedback.
+        • Depending on the location of the passenger, the personal data may be disclosed to Ntuha Services and partners (local subsidiaries, representatives, affiliates, agents etc). Processing of personal data by Ntuha Services and partners will occur under the same conditions as established in this privacy notice.
+        5. Security and access
+        • Any personal data collected in the course of providing the services is transferred to and stored in the data centres of Amazon Web Services, Inc., which are located in the territories of a Member States of the European Union. Only authorised employees of Ntuha Services and partners have access to the personal data and they may access the data only for the purpose of resolving issues associated with the use of the services (including disputes regarding transportation services).
+        • Ntuha Services and partners can access personal data to the extent necessary to provide customer support, see more https://ntuhaug.com
+        • Geolocation data is processed in anonymized form and personalized only if the geographical location data is needed to be linked to a passenger for resolving disputes or fraud.
+        • For research and scientific purposes, the data is used in unidentified form (anonymized)
+        6. Access and correction
+        • You can access and update your personal data via the Ntuha Ride app.
+        7. Retention
+        • Your personal data will be stored as long as you have an active passenger account. If your account is closed, personal data will be deleted (according to the policies set out in this section) from the database, unless such data is required to be retained for accounting, dispute resolution or fraud prevention purposes.
+        • Financial data regarding transportation services provided to passengers will be stored for 4 years after the last journey.
+        • Data required for accounting purposes will be stored for 7 years after the last journey.
+        • In the event that there are suspicions of a criminal offence, fraud or false information having been provided, the data will be stored for 10 years.
+        • In case of payment disputes, data will be retained until the claim is satisfied or the expiry date of such claims.
+        • Journey history data will be stored for 4 years, after which the data will be anonymized.
+        • Please note that the deinstallation of Ntuha Ride app in your device does not cause the deletion of your personal data.
+        • If the Ntuha Ride app has not been used for 3 years, we will notify you and ask you to confirm whether account is still active. If no reply is received, the account will be closed and personal data will be deleted unless such data is required to be stored for accounting, dispute resolution or fraud prevention purposes.
+        8. Deletion
+        • You should also bear in mind that any request to delete your personal data is possible only if we delete your account. As a result of that you will not be able to use Ntuha Ride app via an account which has been deleted.
+        • We respond to any request to delete personal data submitted by e-mail within a month and will specify the period of data deletion.
+        9. Portability
+        • We will respond to any request for transfer of personal data submitted by e-mail within a month and specify when the data transfer will take place. After we have verified the customer in question, we will provide you with your personal data, which includes: contact information, last 4 years' journey history and payment information.
+        10. Direct marketing
+        • We will only use your e-mail address and/ or phone number to send direct marketing messages if you have given us permission to do so via the Ntuha Ride App or Ntuha ride website. We may personalize direct marketing messages using the information by how you use Ntuha Ride services (frequency of use, journeys, payments).
+        • If you no longer wish to receive direct marketing messages, please click the "Unsubscribe" link in the footer of our e-mail.
+        11. Dispute resolution
+        • Disputes relating to the processing of personal data are resolved through customer support (info@ntuhaug.com).
+        • The supervisory authority is the Uganda Registration Services Bureau (www.ursb.co.ug) 
+        </p><br />
+        </div>
+        <button class="collapsible">Ntuha Food App Users' Privacy Policy</button>
+        <div class="content">
+        <p>Your privacy is important to Ntuha and always has been. So, we've developed a Privacy Policy that covers how we collect, use, disclose, transfer, and store your information. Please take a moment to familiarize yourself with our privacy practices and let us know if you have any questions.
+
+        By visiting Ntuha, you are accepting the practices described in this Privacy Notice. 
+
+        1. How Personal Information about Customers is used?
+        User data may be shared with Ntuha providers to improve order processing and customer service. It may also be used both for marketing research purposes and customer relation management, it being specified that some of those providers are not located in the European Union.
+
+        What is done with your personal information?
+        User data collected helps us to personalize our website and Ntuha Food app according to each user’s wishes and preferences. Offering you the most spontaneous and friendly surfing experience is our priority. Data collected are for statistical purposes only and help us to:
+        • Process orders
+        • Deliver products and services
+        • Process payments and communicate with you about your orders, products, services and promotional offers
+        • Keep and update our database and your accounts with us
+        • Propose a unique and targeted navigation experience
+        • Prevent and detect fraud and abuse on our website and Ntuha Food app
+        2. What about Cookies?
+        Cookies are unique identifiers that we transfer to your device to enable our systems to recognize your device and to provide features to make your navigation experience unique and targeted. 
+
+        The acceptance of cookies is not a requirement for visiting the Site. However, we would like to point out that the use of the 'basket' functionality on the Site and ordering is only possible with the activation of cookies. Cookies are tiny text files which identify your computer to our server as a unique user when you visit certain pages on the Site and they are stored by your Internet browser on your computer's hard drive. Cookies can be used to recognize your Internet Protocol address, saving you time while you are on, or want to enter, the Site. We only use cookies for your convenience in using the Site (for example to remember who you are when you want to amend your shopping cart without having to re-enter your email address) and not for obtaining or using any other information about you (for example targeted advertising). Your browser can be set to not accept cookies, but this would restrict your use of the Site. Please accept our assurance that our use of cookies does not contain any personal or private details and are free from viruses. 
+
+        3. What is the accuracy of the Personal Data provided to Ntuha?
+        You declare and guarantee that you are the owner or have the necessary rights on the content that You transmit to us; that at the date of its transmission (i) the content is exact and true, (ii) the use of the content does not contravene any of our policies and will not be damaging to any third party (i.e. that the content is not defamatory).
+        4. How Secure is Information about Me?
+        It is important for you to protect against unauthorized access to your password and to your computer. Be sure to sign off when finished using a shared computer. 
+        We work to protect the security of your information during transmission by using Secure Sockets Layer (SSL) software, which encrypts information you input.
+        We reveal only the last four digits of your credit card numbers when confirming an order. Of course, we transmit the entire credit card number to the appropriate credit card company during order processing. 
+        5. Examples of Information Collected: 
+        • Information you give us: You provide most such information when you search, buy, post, participate in a contest or questionnaire, or communicate with customer service. For example, you provide information when you search for a product; place an order through Ntuha or one of our third-party sellers; provide information in Your Account (and you might have more than one if you have used more than one e-mail address when shopping with us) or Your Profile; communicate with us by phone, e-mail, or otherwise; complete a questionnaire or a contest entry form. As a result of those actions, you might supply us with such information as your name, address, and phone numbers; credit card information; people to whom purchases have been shipped, including addresses and phone number; e-mail addresses of your friends and other people; content of reviews and e-mails to us; personal description and photograph in Your Profile ; and financial information, including driver's license numbers.
+
+        • Automatic information: Examples of the information we collect and analyze include the Internet protocol (IP) address used to connect your computer to the Internet; login; e-mail address; password; computer and connection information such as browser type, version, and time zone setting, browser plug-in types and versions, operating system, and platform; purchase history, which we sometimes aggregate with similar information from other customers to create features like Top Sellers ; the full Uniform Resource Locator (URL) clickstream to, though, and from our Web site, including date and time; cookie number; products you viewed or searched for; and the phone number you used to call our call center number. We may also use browser data such as cookies, Flash cookies (also known as Flash Local Shared Objects), or similar data on certain parts of our Web site for fraud prevention and other purposes. During some visits we may use software tools such as JavaScript to measure and collect session information, including page response times, download errors, length of visits to certain pages, page interaction information (such as scrolling, clicks, and mouse-overs), and methods used to browse away from the page. We may also collect technical information to help us identify your device for fraud prevention and diagnostic purposes.
+
+
+        • Mobile: Most mobile devices provide users with the ability to disable location services. Most likely, these controls are located in the device's settings menu. If you have questions about how to disable your device's location services, we recommend you contact your mobile service carrier or your device manufacturer.
+
+        • Information from other sources: Examples of information we receive from other sources include updated delivery and address information from our carriers or other third parties, which we use to correct our records and deliver your next purchase or communication more easily; account information, purchase or redemption information, and page-view information from some merchants with which we operate co-branded businesses or for which we provide technical, fulfillment, advertising, or other services; search term and search result information from some searches conducted through the Web search features.
+        6. What are your rights?
+        If you are concerned about your data you have the right to request access to the personal data which we may hold or process about you. You have the right to require us to correct any inaccuracies in your data free of charge. At any stage you also have the right to ask us to stop using your personal data for direct marketing purposes.
+        </p><br />
+        </div>
+      </div>
+    </section>
     
     
-    <!--Bottom-->
+     
  
     
     <footer>
@@ -189,6 +317,23 @@
     <script>
       new WOW().init();
     </script>
+
+    <script>
+        var coll = document.getElementsByClassName("collapsible");
+        var i;
+
+        for (i = 0; i < coll.length; i++) {
+          coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+              content.style.display = "none";
+            } else {
+              content.style.display = "block";
+            }
+          });
+        }
+      </script>
     
    
     </body>
