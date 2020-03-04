84611d487ffe82492eb6e9a242549e2ee7b32a57
THEMBO CHARLES LWANGA <ashley7520charles@gmail.com>
2020-03-04 18:29:02 +0300

    Changes front page

---
+++ resources/views/front.blade.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Ntuha deliveries</title>
+	
+	<!-- core CSS -->
+    <link href="/front/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/front/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/front/css/animate.min.css" rel="stylesheet">
+    <link href="/front/css/prettyPhoto.css" rel="stylesheet">
+    <link href="/front/css/main.css" rel="stylesheet">
+    <link href="/front/css/responsive.css" rel="stylesheet">
+    <link rel="shortcut icon" href="/front/images/ntuha.png">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+   
+</head>
+
+<body class="homepage">
+
+    <header id="header">
+        <div class="top-bar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-10 col-xs-10">
+                        <div class="top-number">
+                            <p>
+                                <i class="fa fa-map-marker"></i>&nbsp; &nbsp;Bam Complex Building, Room 12C, High street, Mbarara  &nbsp;&nbsp; 
+                                <i class="fa fa-phone-square"></i>&nbsp;&nbsp; +256704 44 44 81 &nbsp;&nbsp; +256703 03 70 87   &nbsp;&nbsp;
+                                <i class="fa fa-envelope-o"></i> &nbsp;&nbsp; ntuha.deliveries@gmail.com
+                            </p>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div><!--/.container-->
+        </div><!--/.top-bar-->
+
+        <nav class="navbar navbar-inverse" role="banner">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php"><img src="/front/images/ntuha.png" width="40%" alt="logo"></a>
+                </div>
+				              
+            </div><!--/.container-->
+        </nav><!--/nav-->
+		
+    </header><!--/header-->
+
+    <section id="main-slider" class="no-margin">
+        <div class="carousel slide">
+            <ol class="carousel-indicators">
+                <li data-target="#main-slider" data-slide-to="0" class="active"></li>
+                <li data-target="#main-slider" data-slide-to="1"></li>
+                <li data-target="#main-slider" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+
+                <div class="item active" style="background-color: #35D073">
+                    <div class="container">
+                        <div class="row slide-margin">
+                            <div class="col-sm-6">
+                                <div class="carousel-content">
+                                    <h1 class="animation animated-item-1">Farmer uses the platform to search for available buyers for the produce</h1>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 hidden-xs animation animated-item-4">
+                                <div class="slider-img">
+                                    <img src="/front/img/farmer_one.jpg" class="img-responsive">
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div><!--/.item-->
+
+                <div class="item" style="background-color: #35D073">
+                    <div class="container">
+                        <div class="row slide-margin">
+                            <div class="col-sm-6">
+                                <div class="carousel-content">
+                                    <h1 class="animation animated-item-1">Farmer requests for reliable means of transport for the goods to the market</h1></div>
+                            </div>
+
+                            <div class="col-sm-6 hidden-xs animation animated-item-4">
+                                <div class="slider-img">
+                                    <img src="/front/img/farmertwo.PNG" class="img-responsive">
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div><!--/.item--> 
+            </div><!--/.carousel-inner-->
+        </div><!--/.carousel-->
+        <a class="prev hidden-xs" href="#main-slider" data-slide="prev">
+            <i class="fa fa-chevron-left"></i>
+        </a>
+        <a class="next hidden-xs" href="#main-slider" data-slide="next">
+            <i class="fa fa-chevron-right"></i>
+        </a>
+    </section><!--/#main-slider-->
+
+
+
+    <section id="services" class="service-item">
+	   <div class="container">
+            <div class="center wow fadeInDown">
+                <h2>Who we are</h2>
+                <p class="lead">We are a digital platform that connects farmers with ready farm produce buyers, agricultural farm inputs companies, and transporters in order to increase farmers’ income. We have pioneered an alternative system that replaces the use of middlemen in the agricultural ecosystem.  The platform combines a logistics and market facilitation mechanism to enable the farmers to maximize return on their investment. On top of that, farmers also receive climate/seasons information which enables them to plan on when to start planting and what to plant to maximize their agricultural productivity. We have adopted partnerships approach in achieving this goal, such as the partnership with EBBO Sacco, Mbarara to help create awareness of our solution, and also integrate the financial solution.</p>
+            </div>
+ 
+        </div>
+    </section>
+
+    <section id="aboutus" class="service-item">
+       <div class="container">
+            <div class="center wow fadeInDown">
+                <h2>Our solution to farmers</h2>               
+                <p class="lead" style="font-size: 20px;"> Farmer    Platform   Market </p>
+                <p class="lead">Farmer uses the platform to search for available buyers for the produce, requests for reliable means of transport to transport goods to the market and gains high income from selling his produce directly to the buyer</p>
+            </div>
+
+        </div>
+    </section>
+
+     <section id="conatcat-info" class="service-item">
+       <div class="container">
+            <div class="wow fadeInDown">  
+            <center>
+                <h2>Platform Profile</h2> 
+            </center>              
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <h4>Farmer’s profile</h4>
+                         <ul>
+                             <li>Access to Buyers</li>
+                             <li>Access to transportation</li>
+                             <li>Sell farm produce</li>
+                             <li>Access to farm inputs</li>
+                             <li>Access to seasons’ info</li>
+                         </ul>                          
+                         </p>
+                    </div>
+                    <div class="col-md-4">
+                        <h4>Buyers Profile</h4>
+                        <ul>
+                            <li>Access to farmers</li>
+                            <li>Access to bumper harvest information</li>
+                            <li>Buys directly from farmers</li>                            
+                        </ul>
+                    </div>
+
+                     <div class="col-md-4">
+                        <h4>Our customers</h4>
+                        <ul>
+                           <li>100 transporters </li>
+                           <li>1200 farmers onboarded</li>
+                           <li>3 Farm input companies </li>
+                           <li>300 buyers available</li>                            
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    
+ 
+
+ 
+
+    <footer id="footer" class="midnight-blue">
+        <div class="container">
+            
+            <center>
+                <?php echo date("Y") ?> <a href="">Ntuha Deliveries (U) LTD</a>. All Rights Reserved. 
+            </center>
+                 
+        </div>
+    </footer><!--/#footer-->
+
+    <script src="/front/js/jquery.js"></script>
+    <script src="/front/js/bootstrap.min.js"></script>
+    <script src="/front/js/jquery.prettyPhoto.js"></script>
+    <script src="/front/js/jquery.isotope.min.js"></script>
+    <script src="/front/js/main.js"></script>
+    <script src="/front/js/wow.min.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 ini_set('max_execution_time', '1200');
 use App\Http\Controllers\DriverController;
 Route::get('/', function () {
-    return view('welcome');
+    return view('front');
 });
 
 
