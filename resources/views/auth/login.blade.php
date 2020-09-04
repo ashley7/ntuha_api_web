af318fe851d817c23543e70b3700640d9a1f0af1
THEMBO CHARLES LWANGA <ashley7520charles@gmail.com>
2020-09-04 14:59:57 +0300

    Clean ups

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,15 +5,12 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">                
-                <div class="card-body">
-                                    
+                <div class="card-body">                                    
                     <center>
                         <img src="{{asset('logo/logo.png')}}" width="30%">
-                    </center>
-                    
+                    </center>                    
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
