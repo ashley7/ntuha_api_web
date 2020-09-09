e98d73b87e69109e4e35573939040da948ff6a09
THEMBO CHARLES LWANGA <ashley7520charles@gmail.com>
2020-09-09 14:44:03 +0300

    Dashboard

--- resources/views/customer/import_customers.blade.php
+++ resources/views/customer/import_customers.blade.php
@@ -5,17 +5,15 @@
 <h1>Import user accounts</h1>
 	<div class="card">
 		<div class="card-body">
-			<div class="row">
-            	<div class="col-md-6">		
-					<form method="POST" action="/import_customer" enctype="multipart/form-data">
-						{{csrf_field()}}			 
+			<div class="container">			 		
+				<form method="POST" action="/import_customer" enctype="multipart/form-data">
+					{{csrf_field()}}			 
 
-						<input type="file" name="excel_file">
+					<input type="file" name="excel_file">
 
-						<br><br>
-						<button class="btn btn-success">Save</button>
-					</form>
-			    </div>
+					<br><br>
+					<button class="btn btn-success">Save</button>
+				</form>			   
 			</div>			 			
 		</div>
 	</div> 
