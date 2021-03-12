226bfeee120447ff0b75afa995b573b065494df6
THEMBO CHARLES LWANGA <ashley7520charles@gmail.com>
2021-03-12 07:29:08 +0300

    Updates the front page

--- resources/views/front.blade.php
+++ resources/views/front.blade.php
@@ -41,37 +41,25 @@ https://templatemo.com/tm-540-lava-landing-page
     </div>
     <!-- ***** Preloader End ***** -->
 
-
-    <!-- ***** Header Area Start ***** -->
+ <!-- ***** Header Area Start ***** -->
     <header class="header-area header-sticky">
         <div class="container">
             <div class="row">
                 <div class="col-12">
                     <nav class="main-nav">
                         <!-- ***** Logo Start ***** -->
-                        <a href="/" class="logo">
-                           <img src="/assets/images/ntuha.png" alt="NTUHA">
+                        <a href="index.html" class="logo">
+                           <img src="assets/images/ntuha.png" alt="NTUHA">
                         </a>
                         <!-- ***** Logo End ***** -->
                         <!-- ***** Menu Start ***** -->
                         <ul class="nav">
-
-                            <li class="scroll-to-section">
-                                <a href="#welcome" class="menu-item">Home</a>
+                            <li class="scroll-to-section"><a href="#welcome" class="menu-item">Home</a></li>
+                            <li class="scroll-to-section"><a href="#about" class="menu-item">About</a></li>
+                            <li class="scroll-to-section"><a href="#testimonials" class="menu-item">Testimonials</a>
                             </li>
-
-                            <li class="scroll-to-section">
-                                <a href="#about" class="menu-item">About</a>
-                            </li>
-                            <li class="scroll-to-section">
-                                <a href="#testimonials" class="menu-item">Testimonials</a>
-                            </li>
-                            <li>
-                                <a href="https://www.health.go.ug/covid/" target="_blank">
-                                COVID-19</a></li>
+                            <li class="scroll-to-section"><a href="https://www.health.go.ug/covid/" class="menu-item" target="_blank">COVID-19</a></li>
                             <li class="scroll-to-section"><a href="#contact-us" class="menu-item">Contact Us</a></li>
-
-                            <li class="scroll-to-section"><a href="/login">Login</a></li>
                         </ul>
                         <a class='menu-trigger'>
                             <span>Menu</span>
@@ -95,10 +83,8 @@ https://templatemo.com/tm-540-lava-landing-page
                     <div class="left-text col-lg-6 col-md-12 col-sm-12 col-xs-12"
                         data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
                         <h1>We are <em>NTUHA</em></h1>
-                        <p>A mobile and USSD enabled platform that allows <a href="#">farmers, traders, and other users (commuters)</a> to search for available means of
-transport for their agro inputs and produce in a way that makes them gain high income.<a href="#">Using Mobile App and USSD applicatication</a>.
-                           Users can get
-affordable transportation through Ntuha Boda, Ntuha Taxi, and Ntuha Truck to transport their items safely.</p> 
+                        <p>A mobile and USSD enabled platform that allows <a href="#">farmers, traders, and other users (commuters)</a> to search for available means of transport for their agro inputs and produce in a way that makes them gain high income.<a href="#">Using Mobile App and USSD applicatication</a>.
+                           Users can get affordable transportation through Ntuha Boda, Ntuha Taxi, and Ntuha Truck to transport their items safely.</p> 
                         <a href="http://tiny.cc/wywc7y" class="main-button-slider">DOWNLOAD NTUHA RIDE</a>
                         <a href="#" class="main-button-slider">DIAL *284*11#</a>
                     </div>
@@ -109,7 +95,7 @@ affordable transportation through Ntuha Boda, Ntuha Taxi, and Ntuha Truck to tra
     </div>
     <!-- ***** Welcome Area End ***** -->
 
-    <!-- ***** Features Big Item Start ***** -->
+       <!-- ***** Features Big Item Start ***** -->
     <section class="section" id="about">
         <div class="container">
             <div class="row">
@@ -121,7 +107,9 @@ affordable transportation through Ntuha Boda, Ntuha Taxi, and Ntuha Truck to tra
                             <img src="/assets/images/features-icon-1.png" alt="">
                             <h4>Ntuha Boda</h4>
                             <p>A user can choose to use the platform by requesting a boda-boda (motorcycle) as means of transport. The boda option can help both commuters and farmers for transportation.</p>
-                           
+                            <a href="http://tiny.cc/wywc7y" class="main-button">
+                                Download Now
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -133,7 +121,9 @@ affordable transportation through Ntuha Boda, Ntuha Taxi, and Ntuha Truck to tra
                             <img src="/assets/images/features-icon-2.png" alt="">
                             <h4>Ntuha Taxi</h4>
                             <p>Users especially commuters, need a ride from home to work or any destination. They can be able to request for a Taxi/Cab from our mobile app or USSD platform</p>
-                         
+                            <a href="http://tiny.cc/wywc7y" class="main-button">
+                                Download Now
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -145,14 +135,13 @@ affordable transportation through Ntuha Boda, Ntuha Taxi, and Ntuha Truck to tra
                             <img src="/assets/images/features-icon-3.png" alt="">
                             <h4>Ntuha Truck</h4>
                             <p>Farmers especially, you may want to transport your bulk produce or inputs from your farm to the market; perhaps from the input store to your farm. Ntuha Truck is the ideal transportation.</p>
-                            
+                            <a href="http://tiny.cc/wywc7y" class="main-button">
+                                Download Now
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
-            <center>
-                 <a href="http://tiny.cc/wywc7y" class="main-button">Download Now</a>
-            </center>
         </div>
     </section>
     <!-- ***** Features Big Item End ***** -->
@@ -173,8 +162,7 @@ affordable transportation through Ntuha Boda, Ntuha Taxi, and Ntuha Truck to tra
                             <img src="/assets/images/farmer.png" alt="">
                             <div class="text">
                                 <h4>Objective #1</h4>
-                                <p>Connecting farmers and other users to a reliable means of transport to transport either farm
-produce or inputs.</p>
+                                <p>Connecting farmers and other users to a reliable means of transport to transport either farm produce or inputs.</p>
                             </div>
                         </li>
                         <li data-scroll-reveal="enter right move 30px over 0.6s after 0.5s">
@@ -195,8 +183,7 @@ produce or inputs.</p>
                             <img src="/assets/images/logistics.png" alt="">
                             <div class="text">
                                 <h4>Objective #4</h4>
-                                <p>Combine logistics and market facilitation mechanism to enable farmers maximize return on
-investment.</p>
+                                <p>Combine logistics and market facilitation mechanism to enable farmers maximize return on investment.</p>
                             </div>
                         </li>
                         <li data-scroll-reveal="enter right move 30px over 0.6s after 0.6s">
@@ -310,6 +297,7 @@ investment.</p>
     <!-- ***** Testimonials Ends ***** -->
 
 
+    
     <!-- ***** Footer Start ***** -->
     <footer id="contact-us">
         <div class="container">
@@ -318,25 +306,26 @@ investment.</p>
                     <!-- ***** Contact Form Start ***** -->
                     <div class="col-lg-6 col-md-12 col-sm-12">
                         <div class="contact-form">
-                            <form id="contact" action="/contact" method="POST">
-                                @csrf
+                            <form id="contact" action="" method="post">
                                 <div class="row">
                                     <div class="col-md-6 col-sm-12">
                                         <fieldset>
-                                            <input name="name" type="text" id="name" placeholder="Full Name" required style="background-color: rgba(250,250,250,0.3);">
+                                            <input name="name" type="text" id="name" placeholder="Full Name" required=""
+                                                style="background-color: rgba(250,250,250,0.3);">
                                         </fieldset>
                                     </div>
                                     <div class="col-md-6 col-sm-12">
                                         <fieldset>
-                                            <input name="email" type="email" id="email" placeholder="E-Mail Address" required style="background-color: rgba(250,250,250,0.3);">
+                                            <input name="email" type="text" id="email" placeholder="E-Mail Address"
+                                                required="" style="background-color: rgba(250,250,250,0.3);">
                                         </fieldset>
                                     </div>
                                     <div class="col-lg-12">
                                         <fieldset>
-                                            <textarea name="message" rows="6" id="message" placeholder="Your Message" required style="background-color: rgba(250,250,250,0.3);"></textarea>
+                                            <textarea name="message" rows="6" id="message" placeholder="Your Message"
+                                                required="" style="background-color: rgba(250,250,250,0.3);"></textarea>
                                         </fieldset>
                                     </div>
-                                    <hr>
                                     <div class="col-lg-12">
                                         <fieldset>
                                             <button type="submit" id="form-submit" class="main-button">Send Message
@@ -351,25 +340,45 @@ investment.</p>
                     <div class="right-content col-lg-6 col-md-12 col-sm-12">
                         <h2>Areas of <em>Operation</em></h2>
                         <p><h4><em>>Mbarara Office</em></h4>
-                            Ntuha currently runs its operations in Mbarara town with offices at Bam complex Building. The Mbarara office is the headquarters of Ntuha and offers online logistics and package delivery services to both drivers and commuters in Mbarara and surrounding areas. </br>>Physical Address: Bam Complex Building, High street.
-                            </br>>Email: ntuha.deliveries@gmail.com
-                            </br>>Tel: +256 (0) 704 444 481 or +256 (0) 703 03 7087
-                            </br>
+                        Ntuha currently runs its operations in Mbarara town with offices at Bam complex Building. The Mbarara office is the headquarters of Ntuha and offers online logistics and package delivery services to both drivers and commuters in Mbarara and surrounding areas. </br>>Physical Address: Bam Complex Building, High street.
+                        </br>>Email: ntuha.deliveries@gmail.com
+                        </br>>Tel: +256 (0) 704 444 481 or +256 (0) 703 03 7087
+                        </br>
 
-                            <h4><em>>Kabale Office</em></h4>
-                            In an effort to extend the service to other parts of the country, Ntuha opened a new office in Kabale District at the end of July, 2020 along Kabale - Kisoro road. We believe that the extension to Kabale district will enable extend our services to the people (farmers) that most need it and help Ntuha serve south western region better.
-                            </br>
-                            <h4><em>>Mbale & Eastern Uganda Office</em></h4>
-                            Ntuha as part of expansion plan, opened a new office in Mbale City to extend to most parts of Eastern and Karamoja region. This is after the success of business in Mbarara. Mbale office opened up at the beginning of November, 2020.</p>
+                        <h4><em>>Kabale Office</em></h4>
+                        In an effort to extend the service to other parts of the country, Ntuha opened a new office in Kabale District at the end of July, 2020 along Kabale - Kisoro road. We believe that the extension to Kabale district will enable extend our services to the people (farmers) that most need it and help Ntuha serve south western region better.
+                        </br>
+                        <h4><em>>Mbale & Eastern Uganda Office</em></h4>
+                        Ntuha as part of expansion plan, opened a new office in Mbale City to extend to most parts of Eastern and Karamoja region. This is after the success of business in Mbarara. Mbale office opened up at the beginning of November, 2020.</p>
                         <ul class="social">
                             <li><a href="https://www.facebook.com/ntuha/"><i class="fa fa-facebook"></i></a></li>
                             <li><a href="https://twitter.com/ntuharide"><i class="fa fa-twitter"></i></a></li>
-                            <!-- <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
                             <li><a href="#"><i class="fa fa-rss"></i></a></li>
-                            <li><a href="https://ntuhaug.com/"><i class="fa fa-dribbble"></i></a></li> -->
+                            <li><a href="https://ntuhaug.com/"><i class="fa fa-dribbble"></i></a></li>
                         </ul>
                     </div>
                 </div>
+            </div>
+   <!-- Partners -->
+
+               
+
+            <div class="container">
+                <div class="center-heading">
+              <h2>We 100% Believe in <font color="white">Partnerships</font>[SDG 17]</h2>
+
+            <div align="center">
+               <section class="row">
+                  <div class="column"><img src="/assets/Partners/BBA.png"></div>
+                  <div class="column"><img src="/assets/Partners/usaid.png"></div>
+                  <div class="column"><img src="/assets/Partners/innovation-village.png"></div>
+                  <div class="column"><img src="/assets/Partners/grainpulse.jpg"></div>
+                  <div class="column"><img src="/assets/Partners/rotary-mbra.jpg"></div>
+                 
+               </section>
+               </div>
+            </div>
             </div>
             <div class="row">
                 <div class="col-lg-12">
@@ -382,6 +391,8 @@ investment.</p>
             </div>
         </div>
     </footer>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
 
     <!-- jQuery -->
     <script src="/assets/js/jquery-2.1.0.min.js"></script>
