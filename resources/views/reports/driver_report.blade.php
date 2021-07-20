ba9dfa06113d87f6a4dbef7054ca62f5ca2672da
Ashley7 <ashley7520charles@gmail.com>
2021-07-20 11:21:15 +0300

    Cleaning data

--- resources/views/reports/driver_report.blade.php
+++ resources/views/reports/driver_report.blade.php
@@ -84,7 +84,7 @@
           },
           tooltip: {
               headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-              pointFormat: '<tr><td style="padding:0"><b>{point.y:.1f} Ush</b></td></tr>',
+              pointFormat: '<tr><td style="padding:0"><b>{point.y:.0f} Riders</b></td></tr>',
               footerFormat: '</table>',
               shared: true,
               useHTML: true
